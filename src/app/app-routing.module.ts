332ceef8106561248353a9e701633ad173ae2d15
Saptarshi <saptarshi.devnath@gmail.com>
2021-01-31 21:20:19 +0530

    Added navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', pathMatch: 'full', redirectTo: '' },
+  { path: 'resume', pathMatch: 'full', component: ResumeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BackgroundComponent } from './background/background.component';
+import { ResumeComponent } from './resume/resume.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BackgroundComponent,
+    ResumeComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
