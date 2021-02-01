e985f9853731a47e3b6c020ecd4dc53754786184
Saptarshi <saptarshi.devnath@gmail.com>
2021-02-01 21:00:33 +0530

    Working mobile and desktop Navigation

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -5,11 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
+
 export class NavigationComponent implements OnInit {
 
+  navigatorLinkContentElementId: string = "navbarSupportedContent";
+  private readonly toggleSwitchSelector = "#sitenav>button.navbar-toggler";
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleMenu() {
+    if ((document.getElementById(this.navigatorLinkContentElementId) as HTMLElement).classList.contains("show")) {
+      (document.querySelector(this.toggleSwitchSelector) as HTMLElement).click();
+    }
   }
 
 }
